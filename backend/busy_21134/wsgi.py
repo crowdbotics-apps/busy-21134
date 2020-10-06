cc5c0fcd6c50b93df3b5883a2855e9a166fcd307
Crowdbotics <team@crowdbotics.com>
2020-10-06 00:05:45 +0000

    Update models

--- backend/busy_21134/wsgi.py
+++ backend/busy_21134/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'busy_21134.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "busy_21134.settings")
 
 application = get_wsgi_application()
